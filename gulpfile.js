4b2fcdb969554d2bd81a71d11493293952218e80
Patrascu Lucian <patrascu.lucian93@yahoo.com>
2018-03-06 20:00:17 +0200

    Fixed a bug with "About" anchor
    	modified:   dist/galerie.html
    	modified:   dist/iarna.html
    	modified:   dist/index.html
    	modified:   dist/primavara.html
    	modified:   dist/toamna.html
    	modified:   dist/vara.html
    	modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('sass', function() {
 
 // Move the javascript files into our /dist/js folder
 gulp.task('js', function() {
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'src/js/*.js'])
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest("dist/js"))
       .pipe(browserSync.stream());
 });
