4c9e8f5e59bd52163938ca1da8c344470be59442
Patrascu Lucian <patrascu.lucian93@yahoo.com>
2018-03-08 15:29:15 +0200

    Added title and description to the lightbox of iarna.html
    	modified:   src/iarna.html
    	modified:   src/js/index.js
    	modified:   src/scss/main.scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
 
   $(".gallery-img").click(function(event) {
+
+    $(".navbar").slideUp();
+
     $src = $(this).parent().attr("src");
+    $desc = $(this).parent().siblings("p").html();
+    $title = $(this).parent().siblings("h1").html();
 
     if (!$("#light-box").length > 0) {
 
-      $("body").append("<div id='light-box' style='display: none'><img src=''></div>");
+      $("body").append(`<div id='light-box' style='display: none'>
+        <h1></h1>
+        <img src=''><p></p>
+        </div>`);
+
       $("#light-box").fadeIn('fast');
       $("#light-box img").attr("src", $src);
+      $("#light-box p").html($desc);
+      $("#light-box h1").html($title);
+
       event.preventDefault();
 
     } else {
+
       $("#light-box").fadeIn('fast');
       $("#light-box img").attr("src", $src);
+      $("#light-box p").html($desc);
+      $("#light-box h1").html($title);
+
       event.preventDefault();
     }
+
   });
 
   $("body").on("click", '#light-box', function() {
@@ -24,5 +41,8 @@ $(document).ready(function() {
     });
 
     $("#light-box").fadeOut('fast');
+    $(".navbar").slideDown();
+
   });
+
 });
