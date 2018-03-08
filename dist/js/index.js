69a322eadf6e2d8eb3e394bc3941587e087818f4
Patrascu Lucian <patrascu.lucian93@yahoo.com>
2018-03-08 12:12:47 +0200

    Stylized iarna.html with JQuery (added a lightbox)
    	modified:   src/iarna.html
    	modified:   src/js/index.js
    	modified:   src/scss/main.scss

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,28 @@
-$(document).ready( () => {
-  console.log('test');
+$(document).ready(function() {
+
+  $(".gallery-img").click(function(event) {
+    $src = $(this).parent().attr("src");
+
+    if (!$("#light-box").length > 0) {
+
+      $("body").append("<div id='light-box' style='display: none'><img src=''></div>");
+      $("#light-box").fadeIn('fast');
+      $("#light-box img").attr("src", $src);
+      event.preventDefault();
+
+    } else {
+      $("#light-box").fadeIn('fast');
+      $("#light-box img").attr("src", $src);
+      event.preventDefault();
+    }
+  });
+
+  $("body").on("click", '#light-box', function() {
+
+    $('#light-box img').on('click', function(e) {
+      e.stopPropagation();
+    });
+
+    $("#light-box").fadeOut('fast');
+  });
 });
