f8052b18d127c4ced4cf6b548f92362b05af5b19
Patrascu Lucian <patrascu.lucian93@yahoo.com>
2018-04-05 15:38:16 +0300

    Code refactoring and lightbox's style changed
    	modified:   src/craciun.html
    	modified:   src/generale.html
    	modified:   src/js/index.js
    	modified:   src/scss/main.scss
    	modified:   src/tablouri.html

--- dist/js/index.js
+++ dist/js/index.js
@@ -11,71 +11,19 @@ form.addEventListener('submit', function(event){
 
 // JQuery
 $(document).ready(function() {
-
   // Lightbox variables
+
+
   $(".gallery-img").click(function(event) {
-    var $navBar = $(".navbar");
 
-    var $srcSplit = $(this).attr("src").split("-");
-    var $src = $srcSplit[0] + ".jpg";
-
-    var $desc = $(this).parent().siblings("p").html();
-    var $title = $(this).parent().siblings("h1").html();
-
-    var $body = $("body");
-
-    // Light box functionality
-    function lBcontrol() {
-      $navBar.slideUp(300);
-
-      var $lightBox = $("#light-box");
-      var $img = $lightBox.find("img");
-      var $para = $lightBox.find("p");
-      var $heading = $lightBox.find("heading");
-
-      $lightBox.fadeIn('fast');
-      $img.attr("src", $src);
-      $para.html($desc);
-      $heading.html($title);
-
-      event.preventDefault();
-    }
-
-    // if there is no lightbox, insert one
-    if (!$("#light-box").length > 0) {
-      $body.append(`<div id='light-box' style='display: none' class="row align-items-center justify-content-center mx-auto">
-            <button type="button" class="close text-white"><span aria-hidden="true">&times;</span>
-            </button>
-          <div class='col-lg'>
-            <h1></h1>
-            <img src=''>
-          </div>
-          <div class='col-lg col-xl-3'>
-            <p></p>
-          </div>
-        </div>`
-      );
-
-      lBcontrol();
-
-    } else {
-
-      lBcontrol();
-    }
+    $(this).siblings('.light-box').fadeIn(200);
 
   });
 
-  // Light box close handling
-  $("body").on("click", '#light-box', function() {
-
-    $('#light-box img').on('click', function(e) {
-      e.stopPropagation();
-    });
-
-    $("#light-box").fadeOut(300);
-    $(".navbar").slideDown(300);
-
+  $('.light-box__close').click(function (e) {
+    $(this).parent().parent().fadeOut(200);
   });
+
 
   // Smooth anchor scrolling
   $("a[href^=\\#]").click(function(e) {
